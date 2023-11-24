44710f98ae516f67a13d22eeb2c6a0c679cf66bf
Alvaro Graca <alvaro.ps.graca@gmail.com>
2023-11-24 11:58:13 +0000

    letsgoooo

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ import streamlit as st
 import datetime
 import requests
 
-st.set_page_config(page_title="TaxiFareModel LeWagon", page_icon=":world_map:", layout="wide")
+
+st.title(page_title="TaxiFare NYC Model LeWagon", page_icon=":world_map:", layout="wide")
 
 #---HEADER TIME----
-st.subheader("This front queries the Le Wagon [taxi fare model API](https://taxifare.lewagon.ai/predict?pickup_datetime=2012-10-06%2012:10:20&pickup_longitude=40.7614327&pickup_latitude=-73.9798156&dropoff_longitude=40.6513111&dropoff_latitude=-73.8803331&passenger_count=2)")
+st.subheader("This front queries the Le Wagon [Taxi_fare model API](https://taxifare.lewagon.ai/predict?pickup_datetime=2012-10-06%2012:10:20&pickup_longitude=40.7614327&pickup_latitude=-73.9798156&dropoff_longitude=40.6513111&dropoff_latitude=-73.8803331&passenger_count=2)")
+
 #---FORM---
 with st.container():
     st.write("---")
