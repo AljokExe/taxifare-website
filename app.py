069e591c1edcb508dae9f6e6615e4ecf7b1018c5
Alvaro Graca <alvaro.ps.graca@gmail.com>
2023-11-24 12:23:39 +0000

    letsgo

--- app.py
+++ app.py
@@ -13,18 +13,20 @@ st.write("This front queries the Le Wagon [Taxi_fare model API](https://taxifare
 #---FORM---
 with st.container():
     st.write("---")
-    with st.form(key='params_for_api'):
+    left_column, right_column = st.columns(2)
+    with left_column:
+        with st.form(key='params_for_api'):
 
-        pickup_date = st.date_input('pickup datetime', value=datetime.datetime(2012, 10, 6, 12, 10, 20))
-        pickup_time = st.time_input('pickup datetime', value=datetime.datetime(2012, 10, 6, 12, 10, 20))
-        pickup_datetime = f'{pickup_date} {pickup_time}'
-        pickup_longitude = st.number_input('pickup longitude', value=40.7614327)
-        pickup_latitude = st.number_input('pickup latitude', value=-73.9798156)
-        dropoff_longitude = st.number_input('dropoff longitude', value=40.6413111)
-        dropoff_latitude = st.number_input('dropoff latitude', value=-73.7803331)
-        passenger_count = st.number_input('passenger_count', min_value=1, max_value=8, step=1, value=1)
+            pickup_date = st.date_input('pickup datetime', value=datetime.datetime(2012, 10, 6, 12, 10, 20))
+            pickup_time = st.time_input('pickup datetime', value=datetime.datetime(2012, 10, 6, 12, 10, 20))
+            pickup_datetime = f'{pickup_date} {pickup_time}'
+            pickup_longitude = st.number_input('pickup longitude', value=40.7614327)
+            pickup_latitude = st.number_input('pickup latitude', value=-73.9798156)
+            dropoff_longitude = st.number_input('dropoff longitude', value=40.6413111)
+            dropoff_latitude = st.number_input('dropoff latitude', value=-73.7803331)
+            passenger_count = st.number_input('passenger_count', min_value=1, max_value=8, step=1, value=1)
 
-        st.form_submit_button('Make prediction')
+            st.form_submit_button('Make prediction')
 
 params = dict(
     pickup_datetime=pickup_datetime,
